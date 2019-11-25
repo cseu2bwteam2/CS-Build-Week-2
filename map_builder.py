896b4ace6181d76d21a0563c06f4e75f7012dc2c
Haja Andriamaro <haja.andri@gmail.com>
2019-11-25 16:06:53 +0100

    Hittinh the initi end point to start with. Credentials still hard coded

--- map_builder.py
+++ map_builder.py
@@ -1,3 +1,21 @@
 import requests
 import json
+# from dotenv import load_dotenv
+# load_dotenv()
+# import os
 from queue import Queue
+
+
+# token = os.getenv('TOKEN')
+
+def init_room():
+    # Init
+    response = requests.get("https://lambda-treasure-hunt.herokuapp.com/api/adv/init/", json={
+                            "player": "player375"}, headers={'Authorization': "Token 5b68275223b5774b6a277c8a21b18c4629540e1c"})
+    
+
+    data = response.json()
+    print(data)
+
+
+init_room()
