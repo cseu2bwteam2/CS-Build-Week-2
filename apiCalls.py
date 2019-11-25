f21d823b098e0fc0320f65fe2d9c21733b4e4081
Ezekiel Ekunola <ekunolaeasybuoy@gmail.com>
2019-11-25 18:53:26 +0100

    install dependencies

--- apiCalls.py
+++ apiCalls.py
@@ -11,7 +11,6 @@ token = os.environ['TOKEN']
 player = os.environ['PLAYER']
 
 
-
 class APICalls:
     def __init__(self):
         self.player = player
@@ -20,14 +19,14 @@ class APICalls:
         self.status = {}
 
     def init(self):
-        response = requests.get(url + '/adv/init/', headers={'Authorization': token}, json={"player": self.player}).json()
-
-        try: 
+        response = requests.get(
+            url + '/adv/init/', headers={'Authorization': token}, json={"player": self.player}).json()
+       
+        try:
             self.waiting_time = time.time() + float(response.get('cooldown'))
             self.current_room = response
         except:
             print("Invalid response", response)
-
 
     def move(self, dir):
         if self.waiting_time > time.time():
@@ -35,29 +34,34 @@ class APICalls:
         if dir not in self.current_room['exits']:
             print('You cant move')
             return
-        response = requests.post(url + '/adv/move/', headers={'Authorization': token}, json={"direction": dir}).json()
-        try: 
+            print(url)
+        response = requests.post(
+            url + '/adv/move/', headers={'Authorization': token}, json={"direction": dir}).json()
+        try:
             self.waiting_time = time.time() + float(response.get('cooldown'))
             self.current_room = response
             print(self.current_room)
         except:
             print("Invalid response", response)
-    
+
     def take(self, item='tiny_treasure'):
         if self.waiting_time > time.time():
             time.sleep(self.waiting_time - time.time())
         if item not in self.current_room['items']:
             print('No items')
             return
-        response = requests.post(url + '/adv/take/', headers={'Authorization': token}, json={"name": item}).json()
+        response = requests.post(
+            url + '/adv/take/', headers={'Authorization': token}, json={"name": item}).json()
         try:
             print(response)
         except:
             print("Invalid response", response)
+
     def checkStatus(self):
         if self.waiting_time > time.time():
             time.sleep(self.waiting_time - time.time())
-        response = requests.post(url + '/adv/status/', headers={'Authorization': token}).json()
+        response = requests.post(url + '/adv/status/',
+                                 headers={'Authorization': token}).json()
         try:
             print(response)
             self.status = response
@@ -71,7 +75,8 @@ class APICalls:
             print('Unable to sell treasure')
             return
 
-        response = requests.post(url + '/adv/sell/', headers={'Authorization': token}, json={"name": item, 'confirm': 'yes'}).json()
+        response = requests.post(
+            url + '/adv/sell/', headers={'Authorization': token}, json={"name": item, 'confirm': 'yes'}).json()
         try:
             print(response)
             self.status = response
@@ -81,8 +86,9 @@ class APICalls:
     def getBalance(self):
         if self.waiting_time > time.time():
             time.sleep(self.waiting_time - time.time())
-        
-        response = requests.get(url + '/bc/get_balance/', headers={'Authorization': token}).json()
+
+        response = requests.get(url + '/bc/get_balance/',
+                                headers={'Authorization': token}).json()
         try:
             print(response)
         except:
@@ -94,14 +100,15 @@ class APICalls:
         if dir not in self.current_room['exits']:
             print('You cant move')
             return
-        
-        response = requests.post(url + '/adv/move/', headers={'Authorization': token}, json={"direction":dir, "next_room_id": next_room}).json()
+
+        response = requests.post(url + '/adv/move/', headers={'Authorization': token}, json={
+                                 "direction": dir, "next_room_id": next_room}).json()
         try:
             self.waiting_time = time.time() + float(response.get('cooldown'))
             self.current_room = response
         except:
             print("Invalid response", response)
-    
+
     def pray(self):
         if self.waiting_time > time.time():
             time.sleep(self.waiting_time - time.time())
@@ -109,26 +116,24 @@ class APICalls:
         if self.current_room['title'] != 'Shrine':
             print('You are not in a shrine')
             return
-        
-        response = requests.post(url + '/bc/get_balance/', headers={'Authorization': token}).json()
+
+        response = requests.post(
+            url + '/bc/get_balance/', headers={'Authorization': token}).json()
         try:
             print(response)
         except:
             print("Invalid response", response)
 
-    
 
+c = APICalls()
 
-
-# c = APICalls()
-
-# c.init()
-# c.move('s')
-# c.move('e')
-# c.move('w')
-# c.move('d')
-# c.take()
-# c.checkStatus()
-# c.sell()
-# c.getBalance()
-# c.pray()
+c.init()
+c.move('s')
+c.move('e')
+c.move('w')
+c.move('d')
+c.take()
+c.checkStatus()
+c.sell()
+c.getBalance()
+c.pray()
