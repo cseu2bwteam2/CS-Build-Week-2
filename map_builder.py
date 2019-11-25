e774e2ead08df3ae1c305f5483e48de2374ccbb3
Haja Andriamaro <haja.andri@gmail.com>
2019-11-25 17:31:10 +0100

    initialising the rooms_data file with the init end points, rooms direction is intialised with (?) for all directions (n. s, e, w)

--- map_builder.py
+++ map_builder.py
@@ -6,16 +6,27 @@ import os
 from queue import Queue
 
 
+# Read the credential from the .env file
 token = os.getenv('TOKEN')
 player_name = os.getenv('PLAYER_NAME')
+
 
 def init_room():
     # Init
     response = requests.get("https://lambda-treasure-hunt.herokuapp.com/api/adv/init/", json={
                             "player": player_name}, headers={'Authorization': token})
-    
-
     data = response.json()
-    print(data)
+
+    # intialise with empty data
+    rooms_data = []
+    # append with the received data
+    rooms_data.append(data)
+
+    # write the data to the file, it will be used as 
+    # starting point
+    with open("rooms_data.py", "w") as rooms:
+        rooms.write(json.dumps(rooms_data))
+
 
 init_room()
+
